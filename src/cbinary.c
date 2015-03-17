cd9843a4fa170fb4c8334f3541afebca4bb3d18b
Zahari Petkov <zahari@polydraw.com>
2015-03-17 15:41:07 +0200

    Using array defined in Rust from C. Using a function defined in C from Rust.

--- src/cbinary.c
+++ src/cbinary.c
@@ -2,10 +2,27 @@
 
 const char* rust_text();
 
+unsigned char three[3];
+
+
+unsigned int cmax(unsigned int num1, unsigned int num2)
+{
+   unsigned int result;
+
+   if (num1 > num2)
+      result = num1;
+   else
+      result = num2;
+
+   return result;
+}
+
 int main(void) {
    const char* text = rust_text();
 
-   printf(text);
+   unsigned char somewhere = three[0];
+
+   printf("%s - %d\n", text, somewhere);
 
    return 0;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,29 @@
 #![feature(libc)]
 
+#![allow(non_upper_case_globals)]
+
 extern crate libc;
 use std::ffi::CString;
 
 
 #[no_mangle]
+#[repr(C)]
+pub static three: [u8; 3] = [78, 2, 3];
+
+extern {
+   fn cmax(num1: libc::c_uint, num2: libc::c_uint) -> libc::c_uint;
+}
+
+#[no_mangle]
 pub extern fn rust_text() -> *const libc::c_char
 {
-   let to_print = CString::new("Hello from Rust!\n").unwrap();
+   let max_value = unsafe {
+      cmax(40, 80)
+   };
+
+   let s = format!("Hello from Rust - {}!", max_value);
+
+   let to_print = CString::new(s).unwrap();
 
    to_print.as_ptr()
 }
